e60bc77b2e2e987c81cab764160a7191d8907a15
hmmmq <1053825066@qq.com>
2024-01-30 06:30:38 +0800

    恢复

--- flaskProject1/stress_cpu.py
+++ flaskProject1/stress_cpu.py
@@ -1,20 +1,20 @@
 from multiprocessing import Pool, cpu_count
 import time
 
+# Do intensive computation to stress the CPU
 def stress_cpu(n):
     total = 0
     for i in range(n):
-        total += sum(j ** 2 for j in range(1000))   
+        total += i**2
     return total
 
 def main():
     start_time = time.time()
 
-    processes = cpu_count() * 2   
-    large_number = 50000000  
-
+    # Create as many processes as there are CPU cores
+    processes = cpu_count()
     with Pool(processes) as pool:
-        results = pool.map(stress_cpu, [large_number] * processes)
+        results = pool.map(stress_cpu, [110000000, 110000000])
         print(results)
 
     print("time cost: ", time.time() - start_time)
