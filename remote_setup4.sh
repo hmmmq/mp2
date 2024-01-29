784025bd48b0d19c103f4bcfeb061d94564654f4
hmmmq <1053825066@qq.com>
2024-01-30 03:17:57 +0800

    update bash

---
+++ remote_setup4.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 更新和安装必要的包
+sudo apt-get update
+sudo apt-get install -y python3-pip unzip
+sudo pip3 install gunicorn
+
+# 解压 Flask 应用
+git clone https://github.com/hmmmq/mp2.git
+
+# 进入 Flask 应用目录
+cd ~/flaskProject1
+
+# 安装依赖
+pip3 install -r requirements.txt
+
+# 运行 Flask 应用
+gunicorn --bind 0.0.0.0:8000 app:app
